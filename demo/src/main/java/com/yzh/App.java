8431c0ca0af39c9e77dea42d98486ea4fa01272c
wsyzh123 <62328909+wsyzh123@users.noreply.github.com>
2022-09-26 19:11:59 +0800

    Update App.java

--- demo/src/main/java/com/yzh/App.java
+++ demo/src/main/java/com/yzh/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("share");
         System.out.println("test push");
+        System.out.println("test pull");
     }
 }
